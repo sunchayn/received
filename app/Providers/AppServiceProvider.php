9a624171ff097ac3d914ad4b678accc666e470ce
Mazen Touati <mazen_touati@hotmail.com>
2020-04-29 14:32:51 +0100

    Force app URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        \URL::forceRootUrl(config('app.url'));
+        \URL::forceRootUrl(config('app.url'));
+        \URL::forceScheme('https');
     }
 }
